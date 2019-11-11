12dc24e8fb94602625ad63fdb8c05b6a39b6f3c1
Michael Widler <MichaelWidler@Yahoo.com>
2019-11-11 11:51:25 -0800

    Add Ajax and navigation

--- tradewars.js
+++ tradewars.js
@@ -1,24 +1,12 @@
+// init
+setActiveForm("name-form");
+
 // hide inactive forms, show the active one, gray out the unselected buttons and blue the selected one
 function setActiveForm(targetString)
-{/*
+{
 	// hide/grey everything
-	$(".entryForm").hide();
-	$(".navItem").css("background-color", "gray");
-	
-	// show/blue the two you're using
-	if(targetString == 'Exchange')
-	{
-		$("#ExchangeForm").show();
-		$("#NavExchange").css("background-color", "blue");
-	}
-	else if(targetString == 'Add')
-	{
-		$("#AddForm").show();
-		$("#NavAdd").css("background-color", "blue");
-	}
-	else if(targetString == 'Update')
-	{
-		$("#UpdateForm").show();
-		$("#NavUpdate").css("background-color", "blue");
-	}*/
+	$(".formPanel").hide();
+	// show the thing with the matching ID
+	$("#" + targetString).show();
 }
+
