8589a22105675f07612a6cce556bf2e1b92e7be1
Michael Widler <MichaelWidler@Yahoo.com>
2019-11-11 11:14:50 -0800

    Add javascript file

---
+++ tradewars.js
@@ -0,0 +1,24 @@
+// hide inactive forms, show the active one, gray out the unselected buttons and blue the selected one
+function setActiveForm(targetString)
+{/*
+	// hide/grey everything
+	$(".entryForm").hide();
+	$(".navItem").css("background-color", "gray");
+	
+	// show/blue the two you're using
+	if(targetString == 'Exchange')
+	{
+		$("#ExchangeForm").show();
+		$("#NavExchange").css("background-color", "blue");
+	}
+	else if(targetString == 'Add')
+	{
+		$("#AddForm").show();
+		$("#NavAdd").css("background-color", "blue");
+	}
+	else if(targetString == 'Update')
+	{
+		$("#UpdateForm").show();
+		$("#NavUpdate").css("background-color", "blue");
+	}*/
+}
