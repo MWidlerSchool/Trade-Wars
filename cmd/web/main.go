087cdf624dd4835570013e276ed2b395b26419b6
Irish Fagan <IrishFagan@live.com>
2019-11-22 17:38:10 -0800

    Added functionality for port environment variable

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,7 +24,7 @@ func main() {
 	// and the servemux we just created. If http.ListenAndServe() returns an error
 	// we use the log.Fatal() function to log the error message and exit. Note
 	// that any error returned by http.ListenAndServe() is always non-nil.
-	PORT = os.getenv("PORT")
+	var PORT = os.Getenv("PORT")
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":"+PORT, mux)
