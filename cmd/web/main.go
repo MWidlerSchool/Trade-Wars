0bcd94efdb9b9aebd3978f4723ee5dda79b2e717
Taylor Slye <slyet@oregonstate.edu>
2019-11-26 05:35:16 -0800

    try to fix error with undefined funcs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -18,7 +18,7 @@ func main() {
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.Handle("../../ui/static", http.StripPrefix("ui/static"), http.Fileserver(http.dir("static")))
+	mux.Handle("../../ui/static", http.StripPrefix("ui/static"))
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
 	// two parameters: the TCP network address to listen on (in this case ":4000")
