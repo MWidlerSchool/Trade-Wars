71664e355659da48555b5a69fb01f42490306610
Michael Widler <MichaelWidler@Yahoo.com>
2019-12-03 15:38:33 -0800

    Implement validity checking for movement

--- cmd/web/postfunctions.go
+++ cmd/web/postfunctions.go
@@ -3,14 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	//"os"
 	//"github.com/gorilla/sessions"
 )
 
-func NavButtonPressed(playername string, x string, y string) string {
-	str1 := "updatePlayerLoc(" + x + ", " + y + ");"
-	str2 := "setNavMessage('" + playername + " sets course for sector " + x + ", " + y + "');"
-	return str1 + str2
+func NavButtonPressed(playerName string, x string, y string) string {
+	returnStr := "Target sector must be adjacent to current location!"
+	intX, _ := strconv.Atoi(x)
+	intY, _ := strconv.Atoi(y)
+	if shouldAllowMove(playerMap[playerName], intX, intY) {
+		str1 := "updatePlayerLoc(" + x + ", " + y + ");"
+		str2 := "setNavMessage('" + playerName + " sets course for sector " + x + ", " + y + "');"
+		returnStr = str1 + str2
+	}
+	return returnStr
 }
 
 func PostTest(w http.ResponseWriter, r *http.Request) {
