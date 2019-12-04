49879229a278d0b043c4bef9f415c80e3cea2137
Michael Widler <MichaelWidler@Yahoo.com>
2019-12-04 09:17:10 -0800

    Add documentation to player.go

--- cmd/web/player.go
+++ cmd/web/player.go
@@ -9,6 +9,7 @@ type Player struct {
 	Y int
 }
 
+// get the player name from the cookie
 func extractPlayerName(r *http.Request) string {
 	var cookie, err = r.Cookie("Callsign")
 	if err != nil {
@@ -17,6 +18,7 @@ func extractPlayerName(r *http.Request) string {
 	return cookie.Value
 }
 
+// check if a move is legal
 func shouldAllowMove(p Player, x int, y int) bool {
 	if p.X-x <= 1 && p.X-x >= -1 && p.Y-y <= 1 && p.Y-y >= -1 {
 		return true
