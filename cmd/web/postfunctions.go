880cfedc73dbb62cc6b663b5ab8e335d8e5b985d
Michael Widler <MichaelWidler@Yahoo.com>
2019-12-04 09:16:08 -0800

    Add documentation to postfunctions.go

--- cmd/web/postfunctions.go
+++ cmd/web/postfunctions.go
@@ -10,12 +10,14 @@ import (
 
 func NavButtonPressed(playerName string, x string, y string) string {
 	returnStr := "setNavMessage('Target sector must be adjacent to current location!');"
+	// alpha to int conversion returns two vals, so can't be done inline
 	intX, _ := strconv.Atoi(x)
 	intY, _ := strconv.Atoi(y)
 	if shouldAllowMove(playerMap[playerName], intX, intY) {
 		str1 := "updatePlayerLoc(" + x + ", " + y + ");"
 		str2 := "setNavMessage('" + playerName + " sets course for sector " + x + ", " + y + "');"
 		returnStr = str1 + str2
+		// can't assign values to fields while in map
 		p := playerMap[playerName]
 		p.X = intX
 		p.Y = intY
